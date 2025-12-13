1476d8f4b96a453f0c8db371d0b6900f3cf45dab
Pranab Debnath <62995713+alsopranab@users.noreply.github.com>
2025-12-14 00:28:45 +0530

    Update contacts.js

--- assets/js/views/contacts.js
+++ assets/js/views/contacts.js
@@ -1,9 +1,20 @@
 function renderContacts() {
   app.innerHTML = `
-    <h2>Contact</h2>
-    <div class="card">
-      <p><a href="https://github.com/alsopranab" target="_blank">GitHub</a></p>
-      <p><a href="https://linkedin.com/in/pranab-dnath" target="_blank">LinkedIn</a></p>
-    </div>
+    <section class="fade-in">
+      <h2>Contact</h2>
+
+      <div class="card">
+        <p>
+          <a href="https://github.com/alsopranab" target="_blank">
+            GitHub
+          </a>
+        </p>
+        <p>
+          <a href="https://linkedin.com/in/pranab-dnath" target="_blank">
+            LinkedIn
+          </a>
+        </p>
+      </div>
+    </section>
   `;
 }
