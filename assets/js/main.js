134653985cf362c54f76d85eb02969644e1f4c12
Pranab Debnath <62995713+alsopranab@users.noreply.github.com>
2025-12-16 12:50:24 +0530

    Update main.js

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,8 +1,19 @@
 import { registerRoute, navigate } from "./core/router.js";
+
 import { IntroView } from "./views/intro.js";
 import { DashboardView } from "./views/dashboard.js";
+import { ProjectsView } from "./views/projects.js";
+import { ProjectView } from "./views/project.js";
+import { LearningsView } from "./views/learnings.js";
+import { AnalyticsView } from "./views/analytics.js";
+import { ProfilesView } from "./views/profiles.js";
 
 registerRoute("intro", IntroView);
 registerRoute("dashboard", DashboardView);
+registerRoute("projects", ProjectsView);
+registerRoute("project", ProjectView);
+registerRoute("learnings", LearningsView);
+registerRoute("analytics", AnalyticsView);
+registerRoute("profiles", ProfilesView);
 
 navigate("intro");
