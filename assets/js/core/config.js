805049beef32cdf82814b26040d5e7e25fe75769
Pranab Debnath <62995713+alsopranab@users.noreply.github.com>
2025-12-16 12:40:57 +0530

    Create config.js

---
+++ assets/js/core/config.js
@@ -0,0 +1,26 @@
+export const CONFIG = {
+  owner: {
+    name: "Pranab Debnath",
+    designation: "Tech • Data • Analytics",
+  },
+
+  github: {
+    username: "alsopranab",
+    apiBase: "https://api.github.com",
+    cacheTTL: 60 * 60 * 1000 // 1 hour
+  },
+
+  leetcode: {
+    username: "alsopranab",
+    endpoint: "https://leetcode.com/graphql"
+  },
+
+  hackerrank: {
+    username: "alsopranab"
+    // proxy endpoint will be plugged later
+  },
+
+  routing: {
+    defaultRoute: "intro"
+  }
+};
