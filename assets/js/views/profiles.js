7befe30e61426375136ee44ca8c31dbdebaf51b8
Pranab Debnath <62995713+alsopranab@users.noreply.github.com>
2025-12-16 13:17:07 +0530

    Update profiles.js

--- assets/js/views/profiles.js
+++ assets/js/views/profiles.js
@@ -1,26 +1,42 @@
 import { CONFIG } from "../core/config.js";
 
 export async function ProfilesView(container) {
+  const githubUser = CONFIG?.github?.username;
+  const leetcodeUser = CONFIG?.leetcode?.username;
+  const hackerrankUser = CONFIG?.hackerrank?.username;
+
   container.innerHTML = `
     <section>
       <h2>Profiles</h2>
 
       <div>
-        <a href="https://github.com/${CONFIG.github.username}" target="_blank">
-          GitHub
-        </a>
+        ${
+          githubUser
+            ? `<a href="https://github.com/${githubUser}" target="_blank" rel="noopener noreferrer">
+                GitHub
+              </a>`
+            : `<span>GitHub profile not configured</span>`
+        }
       </div>
 
       <div>
-        <a href="https://leetcode.com/${CONFIG.leetcode.username}" target="_blank">
-          LeetCode
-        </a>
+        ${
+          leetcodeUser
+            ? `<a href="https://leetcode.com/${leetcodeUser}" target="_blank" rel="noopener noreferrer">
+                LeetCode
+              </a>`
+            : `<span>LeetCode profile not available</span>`
+        }
       </div>
 
       <div>
-        <a href="https://www.hackerrank.com/${CONFIG.hackerrank.username}" target="_blank">
-          HackerRank
-        </a>
+        ${
+          hackerrankUser
+            ? `<a href="https://www.hackerrank.com/${hackerrankUser}" target="_blank" rel="noopener noreferrer">
+                HackerRank
+              </a>`
+            : `<span>HackerRank profile not available</span>`
+        }
       </div>
     </section>
   `;
