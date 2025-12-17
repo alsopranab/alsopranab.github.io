020841f832825b31e5e2a377f9adedd150d7c201
Pranab Debnath <62995713+alsopranab@users.noreply.github.com>
2025-12-17 21:49:48 +0530

    Create main.js

---
+++ js/main.js
@@ -0,0 +1,7 @@
+const links = document.querySelectorAll('.nav-links a');
+
+links.forEach(link => {
+  if (window.location.href.includes(link.getAttribute('href'))) {
+    link.classList.add('active');
+  }
+});
